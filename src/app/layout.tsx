599c31efb8c9301c3b6610bc99081cb79f316720
Suyesh Badge <suyeshbadge@gmail.com>
2024-12-19 16:41:29 +0530

    Minor change

---
+++ src/app/layout.tsx
@@ -0,0 +1,5 @@
+// Add these meta tags to improve mobile performance
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+  <meta name="theme-color" content="#000000" />
+</head> 
